b4854573ebfb57d544b5d7cad69c77f266e02407
EnMa85 <137387295+EnMa85@users.noreply.github.com>
2024-01-07 08:12:10 +0100

    Delete _footer.php

--- _footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-        <p>Talentform SpA</p>
-        <p>P.IVA / CF 10322191007</p>
-        <p>Via Angelo Bargoni 8, 00153 Rome, Italy</p>
-        <p>Telefono: 06-45474931</p>
-        <p>E-mail: info@talentform.it</p>
-</footer>

e12c5b067a2e63109bf1afaf5695fc4baf17344d
EnMa85 <137387295+EnMa85@users.noreply.github.com>
2024-01-12 16:12:03 +0100

    Update register_sent.php

--- registro_presenze/register_sent.php
+++ registro_presenze/register_sent.php
@@ -17,14 +17,18 @@ if (isset($_POST["send_register"])) {
       $conn->begin_transaction();
 
       // inserts the first lesson state
-      $stmt_ins_stat = $conn->prepare("INSERT INTO stato_presenze (id_lezione, id_corsista, stato, orario_variazione) VALUES (?, ?, ?, ?)");
+      $stmt_ins_stat = $conn->prepare("INSERT INTO stato_presenze (id_lezione, id_corsista, stato, orario_variazione) 
+                                             VALUES (?, ?, ?, ?)");
       // updates the presences alredy sents
-      $stmt_up_stat = $conn->prepare("UPDATE stato_presenze SET stato = ?, orario_variazione = ? WHERE id_corsista = ? AND id_lezione = ?");
+      $stmt_up_stat = $conn->prepare("UPDATE stato_presenze SET stato = ?, orario_variazione = ? 
+                                             WHERE id_corsista = ? AND id_lezione = ?");
 
       // inserts a new absence in the register lesson
-      $stmt_ins_reg = $conn->prepare("INSERT INTO registro_assenze (id_lezione, id_corsista, ora_inizio, ora_fine, note) VALUES (?, ?, ?, ?, ?)");
+      $stmt_ins_reg = $conn->prepare("INSERT INTO registro_assenze (id_lezione, id_corsista, ora_inizio, ora_fine, note) 
+                                             VALUES (?, ?, ?, ?, ?)");
       // updates the end time of a previously saved absence
-      $stmt_up_reg = $conn->prepare("UPDATE registro_assenze SET ora_fine = ? WHERE id_corsista = ? AND id_lezione = ? AND ora_fine = ?");
+      $stmt_up_reg = $conn->prepare("UPDATE registro_assenze SET ora_fine = ? 
+                                             WHERE id_corsista = ? AND id_lezione = ? AND ora_fine = ?");
 
       // for every student reads data from post
       while($count <= $student_number){
@@ -36,22 +40,20 @@ if (isset($_POST["send_register"])) {
          if (isset($_POST['time_'.$count])) {
             $time = $_POST['time_'.$count];
         } 
-       
          if (isset($_POST['note_'.$count])) {
             $note = $_POST['note_'.$count];
         }
-
          // for the first sending, all presence/absence status must be saved in the status table
          if (empty($lesson_status)){
             $stmt_ins_stat->bind_param("iiss", $lesson_id, $student, $status, $time);
             $stmt_ins_stat->execute();
+            
             // in case of absence saves also in the class register
             if ($status == 'assente'){
                $stmt_ins_reg->bind_param("iisss", $lesson_id, $student, $time, $lesson_end, $note);
                $stmt_ins_reg->execute();
             }
-
-         // if the first status of presences has already been sent, saves only the variations
+         // if the first presences state has already been sent, saves only the variations
          } else {
             if ($status != $lesson_status[$count-1]['stato']){
 
@@ -61,14 +63,12 @@ if (isset($_POST["send_register"])) {
                   $stmt_ins_reg->bind_param("iisss", $lesson_id, $student, $time, $lesson_end, $note);
                   $stmt_ins_reg->execute();
                }
-               
                // for absent to present variation, modifies end time of the last line
                else {
                   $status = 'presente';
                   $stmt_up_reg->bind_param("siis", $time, $student, $lesson_id, $lesson_end);
                   $stmt_up_reg->execute();
                }
-
                // for both cases, updates status table
                $stmt_up_stat->bind_param("ssii", $status, $time, $student, $lesson_id);
                $stmt_up_stat->execute();
